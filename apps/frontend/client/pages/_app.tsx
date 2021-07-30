f6c7e52a8747f13bbde54e5491854abed1bd56cb
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-07-30 17:00:54 +0300

    feat(frontend * scss):add style [WiP]
    * add client style
    * add admin style
    
    Closes #6

--- apps/frontend/client/pages/_app.tsx
+++ apps/frontend/client/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
-import './styles.scss';
+import '../styles/styles.scss';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
