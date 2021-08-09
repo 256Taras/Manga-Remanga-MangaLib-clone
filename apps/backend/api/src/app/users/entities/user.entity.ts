7dd12e151e74518486e6ef42c980b2ee50259e1b
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-08-09 23:54:22 +0300

    feat(api*users):add user entity [WiP]
    * add user.entity.ts
    
    #14

--- apps/backend/api/src/app/users/entities/user.entity.ts
+++ apps/backend/api/src/app/users/entities/user.entity.ts
@@ -1,1 +1,42 @@
-export class User {}
+import { BeforeInsert, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { IUser, UserGender } from '@manga/data-access/shared/interfaces';
+
+@Entity('user')
+export class User implements IUser {
+
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column({ unique: true, length: 25 })
+  public username: string;
+
+  @Column()
+  public email: string;
+
+  @Column()
+  public password: string;
+
+  @Column({ type: 'enum', enum: UserGender, default: UserGender.NOT_SPECIFY })
+  public gender: UserGender;
+
+  @BeforeInsert()
+  public emailToLowerCase() {
+    this.email = this.email.toLowerCase();
+  }
+
+  @Column()
+  public imgUrl: string;
+
+  @Column('boolean', { default: false })
+  public isOnline: boolean;
+
+  @Column({ default: '' })
+  public refreshToken?: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  public createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  public updatedAt: Date;
+
+}
