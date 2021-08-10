20842cb9db987bf5966c0e617c8fc95a183d1df1
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-08-10 09:30:18 +0300

    feat(api*user):add DTO[WiP]
    * add @nestjs/mapped-type in package.json
    * add create-user.dto.ts
    
    #14

--- apps/backend/api/src/app/users/dtos/create-user.dto.ts
+++ apps/backend/api/src/app/users/dtos/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { UserGender } from '@manga/data-access/shared/interfaces';
+import { IsEnum, IsString, Length, IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+
+  @IsString()
+  @Length(2, 25)
+  public username: string;
+
+  @IsEmail()
+  public email: string;
+
+  @Length(4, 25)
+  public password: string;
+
+  @IsEnum(UserGender)
+  @IsNotEmpty()
+  public gender: UserGender;
+
+}
