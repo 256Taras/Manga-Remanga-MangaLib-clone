1aaffd46e13d7f4edd0fa807fb557a2bc44278f1
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-08-16 23:53:39 +0300

    feat(api):add cors [WiP]
    * add main.ts add cors
    
    #16

--- apps/backend/api/src/main.ts
+++ apps/backend/api/src/main.ts
@@ -11,11 +11,27 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- app.use(cookieParser());
+  app.use(cookieParser());
+
+
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET, POST, PUT, PATCH, POST, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization,Accept',
+    credentials: true,
+  });
+
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
+  // });
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
